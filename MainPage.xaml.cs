086955a9db90bae205259c24f0f0e65ef29d4ea3
stevepeeters <noreply@github.com>
2023-11-24 17:13:25 +0100

    less minimal, but still not reproducible

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,33 +6,5 @@
         {
             InitializeComponent();
         }
-
-        private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
-        {
-            var control = (BoxView)e.Data.Properties["control"];
-
-            // remove from current layout
-            ((Layout)control.Parent).Children.Remove(control);
-
-            // add to layout it has been dropped on
-            var dgr = (DropGestureRecognizer)sender;
-            var dropLayout = (Layout)dgr.Parent;
-            dropLayout.Children.Add(control);
-        }
-
-        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
-        {
-            var dgr = (DragGestureRecognizer)sender;
-            var boxView = (BoxView)dgr.Parent;
-            boxView.BackgroundColor = Colors.Black;
-            e.Data.Properties.Add("control", boxView);
-        }
-
-        private void DragGestureRecognizer_DropCompleted(object sender, DropCompletedEventArgs e)
-        {
-            var dgr = (DragGestureRecognizer)sender;
-            var boxView = (BoxView)dgr.Parent;
-            boxView.BackgroundColor = Colors.White;
-        }
     }
 }
