4366ec0708fcb33dcf7db9e7a6b584a6af8f4957
stevepeeters <noreply@github.com>
2023-11-24 19:18:06 +0100

    actual repro

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,5 +6,41 @@
         {
             InitializeComponent();
         }
+
+        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+        {
+            var dgr = (DragGestureRecognizer)sender;
+            var boxView = (BoxView)dgr.BindingContext;
+            e.Data.Properties.Add("control", boxView);
+
+            boxView.BackgroundColor = Colors.Black;
+        }
+
+        private void DragGestureRecognizer_DropCompleted(object sender, DropCompletedEventArgs e)
+        {
+            var dgr = (DragGestureRecognizer)sender;
+            var boxView = (BoxView)dgr.BindingContext;
+
+            boxView.BackgroundColor = Colors.White;
+        }
+
+        private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+        {
+            var vm = (ViewModel)BindingContext;
+            var control = (BoxView)e.Data.Properties["control"];
+
+            var dropTarget = ((DropGestureRecognizer)sender).Parent;
+
+            if (dropTarget == topLayout)
+            {
+                if (vm.BottomDraggableViews.Remove(control))
+                    vm.TopDraggableViews.Add(control);
+            }
+            else if (dropTarget == bottomLayout)
+            {
+                if (vm.TopDraggableViews.Remove(control))
+                    vm.BottomDraggableViews.Add(control);
+            }
+        }
     }
 }
---
+++ ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public class ViewModel : BindableObject
+    {
+        public ObservableCollection<View> TopDraggableViews { get; set; } = new ObservableCollection<View>();
+        public ObservableCollection<View> BottomDraggableViews { get; set; } = new ObservableCollection<View>();
+
+        public ViewModel() 
+        {
+            TopDraggableViews.Add(new BoxView { BackgroundColor = Colors.White });
+            TopDraggableViews.Add(new BoxView { BackgroundColor = Colors.White });
+            TopDraggableViews.Add(new BoxView { BackgroundColor = Colors.White });
+        }
+    }
+}
